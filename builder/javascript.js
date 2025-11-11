ba09f3b08eade8b1e74a3d4d63b88436ba17f347
Rose <TV58777@tvdsb.ca>
2025-11-11 10:19:19 -0500

    Textarea Break Tags

--- builder/javascript.js
+++ builder/javascript.js
@@ -13,20 +13,20 @@ document.getElementById("add-chord").onclick = function(){
   const chordInputs = chordBuilder.querySelectorAll("input");
   let node = document.createElement("div");
   node.innerHTML = chordGen(
-    chordInputs[0].value, 
-    chordInputs[1].value,
-    chordInputs[2].value,
-    chordInputs[3].value,
-    chordInputs[4].value,
-    chordInputs[5].value,
-    chordInputs[6].value
+    chordInputs[0].value.trim(), 
+    chordInputs[1].value.trim(),
+    chordInputs[2].value.trim(),
+    chordInputs[3].value.trim(),
+    chordInputs[4].value.trim(),
+    chordInputs[5].value.trim(),
+    chordInputs[6].value.trim()
   );
   chordBuilder.append(node);
   document.getElementById("copy-chords").append(node.innerHTML);
   
   node = document.createElement("span");
   node.classList.add("chord-text");
-  node.innerHTML = chordInputs[0].value;
+  node.innerHTML = chordInputs[0].value.trim();
   document.getElementById("chord-options").append(node);
   node.onclick = function(){
     chordTextInput.value = node.textContent;
@@ -36,19 +36,45 @@ document.getElementById("add-chord").onclick = function(){
 const chordGen = (chord, f6, f5, f4, f3, f2, f1) =>
   `<div class="tab-container"><h3 class="chord-title">${chord}</h3><div class="tab-line">—<span class="fret">${f1}</span>—</div><div class="tab-line">—<span class="fret">${f2}</span>—</div><div class="tab-line">—<span class="fret">${f3}</span>—</div><div class="tab-line">—<span class="fret">${f4}</span>—</div><div class="tab-line">—<span class="fret">${f5}</span>—</div><div class="tab-line">—<span class="fret">${f6}</span>—</div></div>`;
 
+function addBreakTags(text, target){
+  let beg = 0;
+  let str = text;
+  let cont = true;
+  
+  while(cont){
+    let end = str.indexOf("\n\n", beg);
+    let sub = "";
+    if(end == -1){
+      cont = false;
+      sub = str.substring(beg, str.length);
+    }
+    else{
+      sub = str.substring(beg, end);
+    }
+    let node = document.createElement("p");
+    node.innerHTML = sub.replaceAll("\n", "<br>");
+    target.append(node);
+
+    beg = end + 2;
+  }
+}
+
 lyricConvert.onclick = () => {
-  let converted = lyricInput.value;
-  converted = converted.replaceAll("@@p", "</p>");
-  converted = converted.replaceAll("@p", "<p>");
-  converted = converted.replaceAll("@br", "<br>");
-  converted = converted.replaceAll("@@sp", "</span>");
-  converted = converted.replaceAll("@sp", '<span class="chord-text">');
-  converted = converted.replaceAll(/(?:\r\n|\r|\n)/g, "");
-  lyricContainer.innerHTML = converted;
+  const rawText = lyricInput.value;
+  addBreakTags(rawText, lyricContainer);
+
+
+  // converted = converted.replaceAll("@@p", "</p>");
+  // converted = converted.replaceAll("@p", "<p>");
+  // converted = converted.replaceAll("@br", "<br>");
+  // converted = converted.replaceAll("@@sp", "</span>");
+  // converted = converted.replaceAll("@sp", '<span class="chord-text">');
+  // converted = converted.replaceAll(/(?:\r\n|\r|\n)/g, "");
 };
 
 lyricContainer.addEventListener("click", function (e) {
   const range = document.caretRangeFromPoint(e.clientX, e.clientY);
+  //for future, try caretPositionFromPoint?
 
   if (range) {
     previousText = lyricContainer.innerHTML;
